c8395474aad4ef379010244a36cb7aa2388b05aa
Ishan Goel <igoel.mail@gmail.com>
2020-10-08 10:02:19 +0400

    Lots of stuff

--- uniclip.go
+++ uniclip.go
@@ -3,23 +3,54 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
+
+var secondsBetweenChecksForClipChange = 1
+var helpMsg = `Uniclip - Universal Clipboard
+With Uniclip, you can copy from one device and paste on the other.
+
+Usage: uniclip [ <address> | --help/-h ]
+Examples:
+   uniclip                          # start a new clipboard
+   uniclip 192.168.86.24:53701      # join the clipboard at the address - 192.168.86.24:53701
+   uniclip --help                   # print this help message
+
+Running just ` + "`uniclip`" + ` will start a new clipboard.
+It will also provide an address with which you can connect to the clipboard with another device.`
 
 var detectedOs = runtime.GOOS
 var listOfClients = make([]*bufio.Writer, 5)
 var localClipboard string
+var lock sync.Mutex
+
+func main() {
+	if len(os.Args) == 2 {
+		if os.Args[1] == "--help" || os.Args[1] == "-h" {
+			fmt.Println(helpMsg)
+			return
+		}
+		connectToServer(os.Args[1])
+	} else if len(os.Args) == 1 {
+		fmt.Println("Starting a new clipboard!")
+		makeServer()
+	} else {
+		fmt.Println("Too many arguments.\nTo start a new clipboard, use `uniclip`.\nTo connect to a clipboard, use `uniclip <IP>:<PORT>`")
+	}
+}
 
 func makeServer() {
 	l, err := net.Listen("tcp4", "0.0.0.0:")
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	defer l.Close()
@@ -29,15 +60,15 @@ func makeServer() {
 	for {
 		c, err := l.Accept()
 		if err != nil {
-			fmt.Println(err)
+			handleError(err)
 			return
 		}
+		fmt.Println("Connected to a client")
 		go handleClient(c)
 	}
 }
 
 func handleClient(c net.Conn) {
-	fmt.Println("Connected to:", c.RemoteAddr())
 	w := bufio.NewWriter(c)
 	listOfClients = append(listOfClients, w)
 	defer c.Close()
@@ -49,24 +80,23 @@ func connectToServer(address string) {
 	c, err := net.Dial("tcp4", address)
 	defer c.Close()
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
+	fmt.Println("Connected to the clipboard")
 	go monitorSentClips(bufio.NewReader(c))
 	monitorLocalClip(bufio.NewWriter(c))
 }
 
 func monitorLocalClip(w *bufio.Writer) {
 	for {
+		lock.Lock()
 		localClipboard = getLocalClip()
-		// if localClipboard == "" {
-		// 	fmt.Println("PANICCCCCCCCCC AHHHHHHHHHHHH IT IS EMPTY")
-		// }
+		lock.Unlock()
 		sendClipboard(w, localClipboard)
 		for localClipboard == getLocalClip() {
-			time.Sleep(time.Second * 5)
+			time.Sleep(time.Second * time.Duration(secondsBetweenChecksForClipChange))
 		}
-		fmt.Println("detected change in clipboard")
 	}
 }
 
@@ -75,14 +105,14 @@ func monitorSentClips(r *bufio.Reader) {
 	for {
 		s, err := r.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
+			handleError(err)
 			return
 		}
 		if s == "STARTCLIPBOARD\n" {
 			for {
 				s, err = r.ReadString('\n')
 				if err != nil {
-					fmt.Println(err)
+					handleError(err)
 					return
 				}
 				if s == "ENDCLIPBOARD\n" {
@@ -91,11 +121,13 @@ func monitorSentClips(r *bufio.Reader) {
 				}
 				foreignClipboard += s
 			}
+			lock.Lock() // don't let the goroutine that checks for changes do anything
 			setLocalClip(foreignClipboard)
 			localClipboard = foreignClipboard
+			lock.Unlock() // we've made sure the other goroutine won't have a false positive
 			fmt.Println("Copied:" + "\n\"" + foreignClipboard + "\"\n")
 			for i, w := range listOfClients {
-				if w != nil && i != 0 { //don't send to first client, which is this client
+				if w != nil && i != 0 { // don't send to first client, which is this client
 					sendClipboard(w, foreignClipboard)
 				}
 			}
@@ -104,28 +136,17 @@ func monitorSentClips(r *bufio.Reader) {
 	}
 }
 
-func main() {
-	if len(os.Args) == 2 {
-		connectToServer(os.Args[1])
-	} else if len(os.Args) == 1 {
-		fmt.Println("Starting a new clipboard!")
-		makeServer()
-	} else {
-		fmt.Println("Too many arguments.\nTo start a new clipboard, use `uniclip`.\nTo connect to a clipboard, use `uniclip <IP>:<PORT>`")
-	}
-}
-
 func sendClipboard(w *bufio.Writer, clipboard string) {
 	var err error
 	clipString := "STARTCLIPBOARD\n" + clipboard + "\nENDCLIPBOARD\n"
 	_, err = w.WriteString(clipString)
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	err = w.Flush()
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 }
@@ -133,14 +154,22 @@ func sendClipboard(w *bufio.Writer, clipboard string) {
 func getLocalClip() string {
 	var out []byte
 	var err error
+	var cmd *exec.Cmd
+	errMsg := "An error occurred wile getting the local clipboard"
 	if detectedOs == "darwin" {
-		out, err = exec.Command("pbpaste").Output()
+		cmd = exec.Command("pbpaste")
 	} else if detectedOs == "windows" {
-		out, err = exec.Command("powershell.exe", "-command", "Get-Clipboard").Output()
+		cmd = exec.Command("powershell.exe", "-command", "Get-Clipboard")
 	}
-	if err != nil {
-		fmt.Println(err)
-		return ""
+	if out, err = cmd.CombinedOutput(); err != nil {
+		handleError(err)
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			fmt.Println(string(exiterr.Stderr))
+		}
+		return errMsg
+	}
+	if detectedOs == "windows" {
+		return strings.TrimSuffix(string(out), "\n") // get-clipboard adds a newline to the end for some reason
 	}
 	return string(out)
 }
@@ -154,24 +183,23 @@ func setLocalClip(s string) {
 	}
 	in, err := copyCmd.StdinPipe()
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
-
 	if err := copyCmd.Start(); err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	if _, err := in.Write([]byte(s)); err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	if err := in.Close(); err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	if err := copyCmd.Wait(); err != nil {
-		fmt.Println(err)
+		handleError(err)
 		return
 	}
 	return
@@ -181,9 +209,19 @@ func getOutboundIP() net.IP {
 	// https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go/37382208#37382208
 	conn, err := net.Dial("udp", "8.8.8.8:80") // address can be anything. Doesn't even have to exist
 	if err != nil {
-		fmt.Println(err)
+		handleError(err)
+		return nil
 	}
 	defer conn.Close()
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP
+}
+
+func handleError(err error) {
+	if err == io.EOF {
+		fmt.Println("Disconnected from a device")
+	} else {
+		fmt.Println("An error occurred:", err)
+	}
+	return
 }
